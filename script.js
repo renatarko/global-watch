75de6b40afb43f0517bed3f488ac8d15a03933f6
Renata Karonlina <renatakarolinarko@gmail.com>
2022-07-29 11:11:22 -0400

    coding the feature of the clock

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("input");
+const button = document.querySelector(".btn_sub");
+
+const url = "http://worldtimeapi.org/api/timezone/America/Argentina/Salta";
+
+console.log(url);
